ad713bc69d18425e0ffc7aac8d6d0aa5835e1ec3
unknown <artimushka@gmail.com>
2023-05-18 16:40:55 +0600

    Work section

---
+++ components/Coursework/Coursework.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Coursework.module.scss';
+
+const Coursework = () => {
+    return (
+        <div>
+            <div className={s.coursework}>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Coursework;
