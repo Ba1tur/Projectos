1f2c1bbadb21a34b5de0a443fefc67d7cb22fbd6
unknown <artimushka@gmail.com>
2023-05-23 17:05:59 +0600

    section review

--- components/Reviews/Reviews.jsx
+++ components/Reviews/Reviews.jsx
@@ -3,17 +3,38 @@ import s from "./Reviews.module.scss";
 
 const Reviews = () => {
   return (
-    <div className="container">
-      <div className={s.reviews}>
-        <div className={s.rewievs_title}>
-          <h4>Видео-отзывы</h4>
-        </div>
-        <div className={s.reviews_background_elements}>
-          <div className={s.background_left_element}>
-            <img src="/Dots.png" alt="dots" />
+    <div className="reviews_background">
+      <div className="container">
+        <div className={s.reviews}>
+          <div className={s.rewievs_title}>
+            <h4>Видео-отзывы</h4>
           </div>
-          <div className={s.background_right_element}>
-            <img src="/Waves.png" alt="waves" />
+          <div className={s.reviews_background_elements}>
+            <div className={s.background_left_element}>
+              <img src="/Dots.png" alt="dots" />
+            </div>
+            <div className={s.background_right_element}>
+              <img src="/Waves.png" alt="waves" />
+            </div>
+          </div>
+          <div className={s.reviews_videos}>
+            <div className={s.reviews_video_elements}>
+              <img src="/video.png" alt="video" />
+            </div>
+            <div className={s.reviews_video_elements}>
+              <img src="/video.png" alt="video" />
+            </div>
+            <div className={s.reviews_video_elements}>
+              <img src="/video.png" alt="video" />
+            </div>
+          </div>
+          <div className={s.reviews_small_title}>
+            <h3>
+              Оставь заявку и выбери лучшего автора на основании отзывов
+            </h3>
+          </div>
+          <div className={s.reviews_btn}>
+            <button>Разместить заказ</button>
           </div>
         </div>
       </div>
