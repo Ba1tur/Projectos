68d8197b1f435670cc939dc6c0ef70b5562de1da
Artyomsheq <artimushka@gmail.com>
2023-06-03 03:01:21 +0600

    rework

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./Footer.module.scss";
 import Link from "next/link";
-import { about, directions, informations, support } from "@/constans/Footer";
+import { about, directions, informations, payments, support } from "@/constans/Footer";
 
 const Footer = () => {
   const phoneNumber = "+88001000320";
@@ -9,7 +9,73 @@ const Footer = () => {
 
   return (
     <div className="container">
-      
+      <div className={s.footer}>
+        <div className={s.footer_contacts}>
+          <div className={s.footer_contacts_app}>
+            <div className={s.contacts_app_navigation}>
+              <Link href="#">8-800-100-03-20</Link>
+              <p>Работаем по будням с 10:00 до 20:00</p>
+              <Link href="#">support@author24.ru</Link>
+            </div>
+            <div className={s.contacts_app_btns}>
+              <button><img src="/appStore.png" alt="appStore" /></button>
+              <button><img src="/googlePlay.png" alt="googlePlay" /></button>
+            </div>
+          </div>
+          <div className={s.footer_support}>
+            <div className={s.footer_support_main}>
+              <h3>Поддержка</h3>
+            </div>
+            <div className={s.footer_support_title}>
+              {support.map((item) => (
+                <p key={item.id}>{item.title}</p>
+              ))}
+            </div>
+          </div>
+          <div className={s.footer_support}>
+            <div className={s.footer_support_main}>
+              <h3>Информация</h3>
+            </div>
+            <div className={s.footer_support_title}>
+              {informations.map((item) => (
+                <p key={item.id}>{item.title}</p>
+              ))}
+            </div>
+          </div>
+          <div className={s.footer_support}>
+            <div className={s.footer_support_main}>
+              <h3>Основные направления</h3>
+            </div>
+            <div className={s.footer_support_title}>
+              {directions.map((item) => (
+                <p key={item.id}>{item.title}</p>
+              ))}
+            </div>
+          </div>
+          <div className={s.footer_support}>
+            <div className={s.footer_support_main}>
+              <h3>О компании</h3>
+            </div>
+            <div className={s.footer_support_title}>
+              {about.map((item) => (
+                <p key={item.id}>{item.title}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={s.footer_copyright}>
+          <div className={s.footer_copyright_title}>
+            <h3>© 2012 — 2022 Avtor24 Все права защищены</h3>
+          </div>
+          <div className={s.footer_copyright_payments}>
+            {payments.map((item) => (
+              <div>
+                <img key={item.id} src={item.img} alt="payments" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- constans/Footer.js
+++ constans/Footer.js
@@ -1,120 +1,126 @@
 export const support = [
   {
     id: 1,
-    main: "Поддержка",
-  },
-  {
-    id: 2,
     title: "Telegram",
   },
   {
-    id: 3,
+    id: 2,
     title: "Viber",
   },
   {
-    id: 4,
+    id: 3,
     title: "Facebook",
   },
   {
-    id: 5,
+    id: 4,
     title: "WhatsApp",
   },
   {
-    id: 6,
+    id: 5,
     title: "Email ",
   },
   {
-    id: 7,
+    id: 6,
     title: "Вконтакте",
   },
 ];
 export const informations = [
   {
     id: 1,
-    main: "Информация",
-  },
-  {
-    id: 2,
     title: "Топ экспертов",
   },
   {
-    id: 3,
+    id: 2,
     title: "Правила",
   },
   {
-    id: 4,
+    id: 3,
     title: "Статьи",
   },
   {
-    id: 5,
+    id: 4,
     title: "Помощь",
   },
   {
-    id: 6,
+    id: 5,
     title: "Лента заказов",
   },
   {
-    id: 7,
+    id: 6,
     title: "Справочники",
   },
 ];
 export const directions = [
   {
     id: 1,
-    main: "Основные направления",
-  },
-  {
-    id: 2,
     title: "Работа онлайн-экспертом",
   },
   {
-    id: 3,
+    id: 2,
     title: "Работа репетитором",
   },
   {
-    id: 4,
+    id: 3,
     title: "Работа для преподавателей",
   },
   {
-    id: 5,
+    id: 4,
     title: "Заработок для студентов",
   },
   {
-    id: 6,
+    id: 5,
     title: "Заказ дипломной работы",
   },
   {
-    id: 7,
+    id: 6,
     title: "Примеры студенческих работ",
   },
 ];
 export const about = [
   {
     id: 1,
-    main: "О компании",
-  },
-  {
-    id: 2,
     title: "Отзывы об Автор24",
   },
   {
-    id: 3,
+    id: 2,
     title: "Правообладателям",
   },
   {
-    id: 4,
+    id: 3,
     title: "Популярные вопросы",
   },
   {
-    id: 5,
+    id: 4,
     title: "Партнерская программа",
   },
   {
-    id: 6,
+    id: 5,
     title: "Карта сайта",
   },
   {
-    id: 7,
+    id: 6,
     title: "Контакты",
   },
 ];
+export const payments = [
+  {
+    id: 1,
+    img: "/webMoney.png"
+  },
+  {
+    id: 2,
+    img: "/yandexMoney.png"
+  },
+  {
+    id: 3,
+    img: "/masterCard.png"
+  },
+  {
+    id: 4,
+    img: "/visa.png"
+  },
+  {
+    id: 5,
+    img: "/qiwi.png"
+  },
+]
