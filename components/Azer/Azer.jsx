5932ea8c2ef9ed827aa9b0be133f7072740fabad
karimitacademy <nkavkazka123@gmail.com>
2023-06-07 17:41:19 +0600

    last animation

--- components/Azer/Azer.jsx
+++ components/Azer/Azer.jsx
@@ -18,12 +18,24 @@ const Azer = () => {
       transition: { delay: custom * 0.2 },
     }),
   };
+  const textAnimationTop = {
+    hidden: {
+      y: -100,
+      opacity: 0,
+    },
+    
+    visible: (custom) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2 },
+    }),
+  };
 
   return (
     <motion.div initial="hidden" whileInView="visible" className={s.azer_section}>
       <div className="container">
         <div className={s.azer}>
-          <motion.h2 custom={1} variants={textAnimationBottom} className={s.azer__title}>Другие виды работ</motion.h2>
+          <motion.h2 custom={1} variants={textAnimationTop} className={s.azer__title}>Другие виды работ</motion.h2>
           <motion.div custom={2} variants={textAnimationBottom}  className={s.azer__card}>
             {azers.map((azers) => (
               <div key={azers.id} className={s.azer__card_desc}>
