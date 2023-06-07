78fa9feff9a40cf2010cadbcedbe2ca84c9f4c7b
karimitacademy <nkavkazka123@gmail.com>
2023-06-07 16:48:22 +0600

    animation

--- components/Answers/Answers.jsx
+++ components/Answers/Answers.jsx
@@ -2,24 +2,75 @@ import React from "react";
 import s from "./Answers.module.scss";
 import { answers } from "@/constans/answers";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Answers = () => {
+  const textAnimation = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      x: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.1 },
+    }),
+  };
+  const textAnimationBottom = {
+    hidden: {
+      y: 100,
+      opacity: 0,
+    },
+    
+    visible: (custom) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2 },
+    }),
+  };
+  const textAnimationTop = {
+    hidden: {
+      y: -150,
+      opacity: 0,
+    },
+    
+    visible: (custom) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2 },
+    }),
+  };
+
   return (
-    <div className="container">
+    <motion.div initial="hidden" whileInView="visible" className="container">
       <div className={s.answer}>
         <div className={s.answer__card}>
-          <h2 className={s.answer__card_title}>Тебе могут быть полезны</h2>
+          <motion.h2
+            variants={textAnimationTop}
+            className={s.answer__card_title}
+          >
+            Тебе могут быть полезны
+          </motion.h2>
         </div>{" "}
-        <div className={s.answer__del_card}>
+        <motion.div custom={2} variants={textAnimationBottom} className={s.answer__del_card}>
           {answers.map((answers) => (
             <div key={answers.id} className={s.answer__del_card__info}>
-              <Image src={answers.img} width={100} height={100} alt="notebook" />
+              <Image
+                src={answers.img}
+                width={100}
+                height={100}
+                alt="notebook"
+              />
               <h2>{answers.title}</h2>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- components/Azer/Azer.jsx
+++ components/Azer/Azer.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import s from "./Azer.module.scss";
 import {  azers } from "@/constans/azer";
+import { motion } from "framer-motion";
 
 
 const Azer = () => {
+  
+  const textAnimationBottom = {
+    hidden: {
+      y: 100,
+      opacity: 0,
+    },
+    
+    visible: (custom) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2 },
+    }),
+  };
+
   return (
-    <div className="azer_section">
+    <motion.div initial="hidden" whileInView="visible" className={s.azer_section}>
       <div className="container">
         <div className={s.azer}>
-          <h2 className={s.azer__title}>Другие виды работ</h2>
-          <div className={s.azer__card}>
+          <motion.h2 custom={1} variants={textAnimationBottom} className={s.azer__title}>Другие виды работ</motion.h2>
+          <motion.div custom={2} variants={textAnimationBottom}  className={s.azer__card}>
             {azers.map((azers) => (
               <div key={azers.id} className={s.azer__card_desc}>
                 <div>
@@ -20,10 +35,10 @@ const Azer = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
